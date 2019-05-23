74ae3a23e08bdda49c89756a657f067090c8f932
Flavio Miranda <flavioms7@hotmail.com>
2019-05-23 10:52:53 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/apinewuserlogin/apinewuserlogin/ApinewuserloginApplication.java
@@ -0,0 +1,13 @@
+package com.apinewuserlogin.apinewuserlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApinewuserloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApinewuserloginApplication.class, args);
+	}
+
+}
